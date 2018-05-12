0a49dce55cac46c618a823124ae06856543fb2f0
henry <siauhenry@gmail.com>
2018-05-11 23:59:16 -0700

    feat: add new member info to redux
     while receive new member info from websocket

--- src/reducers/selectedTrip.js
+++ src/reducers/selectedTrip.js
@@ -1,15 +1,7 @@
-import { UPDATE_SELECTED_TRIP, SET_INVITE_CODE, REMOVE_INVITE_CODE } from '../actions/actionTypes';
-const initialState = {
-    tripId: '',
-    title: '',
-    description: '',
-    owner: '',
-    members: [],
-    startDate: '',
-    endDate: '',
-    invitationCode: ''
-}
-export const selectedTrip = (state = initialState, action) => {
+import { UPDATE_SELECTED_TRIP, SET_INVITE_CODE, REMOVE_INVITE_CODE, ADD_MEMBER } from '../actions/actionTypes';
+import settings from '../config';
+
+export const selectedTrip = (state = '', action) => {
     switch (action.type) {
         case UPDATE_SELECTED_TRIP:
             return {
@@ -21,6 +13,22 @@ export const selectedTrip = (state = initialState, action) => {
                 startDate: action.startDate,
                 endDate: action.endDate,
                 invitationCode: action.invitationCode
+            }
+        case ADD_MEMBER:
+            console.log('add member');
+            return {
+                tripId: state.tripId,
+                title: state.title,
+                description: state.description,
+                owner: state.owner,
+                members: state.members.set(action.member.userId, {
+                    userName: action.member.userName,
+                    email: action.email,
+                    profilePictureUrl: action.profilePicture ? settings.serverUrl + action.profilePicture : action.facebookProfilePictureURL || settings.defaultProfilePictureUrl,
+                }),
+                startDate: state.startDate,
+                endDate: state.endDate,
+                invitationCode: state.invitationCode
             }
         default:
             return state
